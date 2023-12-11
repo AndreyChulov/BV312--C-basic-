b97346c3f65d3f9a3d0bf12a473013dc41b87e8b
AndreyChulov <andreyc253@gmail.com>
2023-12-12 00:12:53 +0300

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,198 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main_calc()
+{	
+    setlocale(LC_ALL, "Russian");
+
+    enum {
+        UP = 'w',
+        DOWN = 's',
+        LEFT = 'a',
+        RIGHT = 'd'
+    };
+        
+    char ch{};
+    std::cout << "Enter action (w,a,s,d): ";
+    std::cin >> ch;
+        
+    switch (ch)
+    {
+    case UP: // if (ch == '+')   
+        cout << "Робот идет вверх" << endl;
+        break;    
+    case DOWN:
+        cout << "Робот идет вниз" << endl;
+        break;    
+    case RIGHT:
+        cout << "Робот идет на право" << endl;
+        break;
+    case LEFT:
+        cout << "Робот идет на лево" << endl;
+        break;
+   /* default:
+        std::cout << "Error !!! " << ch << " \n";
+        break;*/
+    }
+    return 0;	
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    /*int i{ 0 };
+
+    while (i<=20)
+    {
+        cout << i << " ";
+        if (i % 2 == 0)
+            cout << "*" << endl;
+        else
+            cout << "-" << endl;
+        i++;
+    }*/
+
+    /*
+    Пользователь с клавиатуры вводит целое число.
+    Необходимо посчитать сумму его цифр.
+    */
+
+    //int a;
+    //cout << "Ведите число ";
+    //cin >> a;
+
+    //int res{};
+
+    //while (a != 0) {
+    //    res += a % 10; // res = res + a%10
+    //    a /= 10;
+    //}
+    //cout << res << endl;
+     
+    /*
+    Написать программу, выводящую на экран линию 
+    из заданного количества  заданных символов.
+    */
+
+    /*int a;
+    cout << "Введите число ";
+    cin >> a;
+
+    char ch;
+    cout << "Введите символ ";
+    cin >> ch;
+
+    while (a!=0)
+    {
+        cout << ch;
+        a--;
+    }*/
+
+
+    // Программа про оценки
+
+    /*int count;
+    cout  << "Введите количество оценок ";
+    cin >> count;    
+    double sum{};
+    int i{ 1 };
+
+    while (i  <= count) {               
+        int av;
+        cout << "Введите оценку " << i <<" ";
+        cin >> av;
+        i++;
+        sum += av;
+    }
+
+
+    cout << "Средняя = " << sum / count;
+    */
+
+    // Программа про оценки 2
+
+    /*int mark{ 0 };
+    double sum{};
+    int i{ 0 };
+    do {
+        cout << "Введите оценку " << i+1 << " :";
+        cin >> mark;
+        sum += mark;
+
+        if (mark != 0)
+            i++;
+        else
+            break;
+
+    } while (true);
+
+    cout << "Средняя = " << sum / i;*/
+
+    //Программа с выводом кратного 5  от 0 до 100 
+
+
+    /*int a{1};
+
+    int i{ 0 };
+
+    while (a <=100) {        
+        if (a% 5 == 0)
+            cout << a << endl;
+        a++;
+        i++;
+    }
+    cout << "Steps = " << i<< endl;
+
+    a = 5;
+    i = 0;
+
+    while (a <= 100) {     
+       cout << a << endl;
+        a+=5;
+        i++;
+    }
+    cout << "Steps = " << i<< endl;
+
+    i = 99999;
+    double r{};
+
+    for (int a = 5; a <= 100; r++, a += 5) {
+        cout << a << endl;    
+    }
+
+    cout << "Steps = " << i << endl;
+    cout << "Steps = " << r << endl;
+
+
+    for (; a != 121; a++) {
+        if (a % 2)
+            continue;
+        cout << a << endl;
+    }*/
+
+    /*
+    У швеи имеется ткань длиной L. 
+    Ей необходимо пошить подушки длиной P. 
+    Условно договоримся, что ширина ткани совпадает
+    с шириной подушки.Определить сколько подушек сможет 
+    пошить швея, если умножать и делить она не умеет.
+    */
+
+    int P{};
+    int L{};
+    cout << "Введите длинну ткани: ";
+    cin >> L;
+    cout << "Введите длинну подушки: ";
+    cin >> P;
+
+    int count{};
+    for (; L > 0; L -= P+P) {
+        count++;
+    }
+    cout << count << endl;
+
+
+    return 0;
+}
---
+++ Lesson2/Lesson2/Lesson2.cpp
@@ -0,0 +1,110 @@
+﻿// Lesson2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+void Task1() {
+    int chislo;
+    std::cout << "Введите число с дробной частью в 1 цифру без запятых:>";
+    std::cin >> chislo;
+
+    std::cout << "Введенное число :>" << chislo / 10 << "." << chislo % 10;
+}
+
+void Task2() {
+    int chislo;
+    std::cout << "Введите число с дробной частью в 2 цифры без запятых:>";
+    std::cin >> chislo;
+
+    std::cout << "Введенное число :>" << chislo / 100 << "." << chislo % 100;
+}
+
+void Task2_0() {
+    int chislo;
+
+    std::cout << "Введите число:>";
+    std::cin >> chislo;
+
+    if (chislo > 10) {
+        std::cout << "Число больше 10";
+    }
+    else {
+        std::cout << "Число меньше или равно 10";
+    }
+}
+
+void Task2_1() {
+    int chislo;
+    std::cout << "Введите число с дробной частью в 2 цифры без запятых:> ";
+    std::cin >> chislo;
+
+    //X.ZZ or X.YZ
+    //X - digit to integer part
+    //Y - potential loosing zero (Y=Z if Y = [1..9])
+    //Z - digit to fractional part
+
+    if (chislo % 100 < 10) {
+        std::cout << "Введенное число :> " << chislo / 100 << ".0" << chislo % 100;
+    }
+    else {
+        std::cout << "Введенное число :> " << chislo / 100 << "." << chislo % 100;
+    }
+}
+
+#include <cstring>
+
+void Task2_2(int digitCount) {
+    int chislo;
+    std::cout << "Введите число с дробной частью :> ";
+    std::cin >> chislo;
+    int delitel = 1;
+
+    for (int count = 0; count < digitCount; count++) {
+        delitel *= 10;
+    }
+
+    int celoe = chislo / delitel;
+    int fractal = chislo % delitel;
+
+    char middle[100] = ".";
+
+    int tempDelitel = delitel;
+    int temp = fractal;
+    int count = 1;
+    while (fractal != 0)
+    {
+        if (temp <= (tempDelitel / 10 - 1)) {
+            middle[count] = '0';
+            count++;
+        }
+        else {
+            break;
+        }
+
+        tempDelitel /= 10;
+    }
+    middle[count + 1] = 0;
+
+
+    std::cout << "Введенное число :> " << celoe << middle << fractal;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Task2_2(5);
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
